02e578bee1d04396df4fb92f5bba5a6c8646d7c5
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-02-02 10:37:19 -0800

    Recommit lost javascript

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
-let search = document.querySelector(".search-button");
-function duh() {
-  let search = document.querySelector(".search-bar").value;
-  let list = document.getElementById("intro");
-  let h2 = document.createElement("h1");
-  h2.textContent = search;
-  h2.classList = "game-desc"; // Remove the dot before "game-desc"
-  list.appendChild(h2);
+<<<<<<< HEAD
+const baseUrl = "https://api.rawg.io/api/games/";
+const apiKey = "?key=652dc6a240454ec7a98d610a0041a14e";
+
+console.log(baseUrl + 54 + apiKey);
+
+// search.addEventListener("click", duh);
+
+// fetchData(gameName);
+
+//Get the game data
+async function fetchGameData(gameName) {
+  const response = await fetch(
+    `https://api.rawg.io/api/games?key=652dc6a240454ec7a98d610a0041a14e&search=${gameName}&parent_platforms=1`
+  );
+  const data = await response.json();
+
+  games = data.results;
+  return games;
 }
-search.addEventListener("click", duh);
+
+async function loopData(gameName) {
+  const gameData = await fetchData(gameName);
+
+  gameData.forEach((game) => {
+    console.log(game);
+  });
+}
+
+loopData("little big planet");
+// let search = document.querySelector(".search-button");
+// function duh() {
+//   let search = document.querySelector(".search-bar").value;
+//   let list = document.getElementById("intro");
+//   let h2 = document.createElement("h1");
+//   h2.textContent = search;
+//   h2.classList = "game-desc"; // Remove the dot before "game-desc"
+//   list.appendChild(h2);
+// }
+=======
+let searchButton = document.querySelector(".search-button");
+const introList = document.getElementById("intro");
+
+searchButton.addEventListener("click", () => {
+  const searchText = document.querySelector(".search-bar").value.trim();
+
+  if (searchText) {
+    const h2 = document.createElement("h1");
+    h2.textContent = searchText;
+    h2.classList.add("game-desc");
+    introList.appendChild(h2);
+  }
+});
+>>>>>>> 6633beaa35342accf909f16da5749a672e20c468
