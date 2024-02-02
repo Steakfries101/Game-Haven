2941c5e70b65c3e8ad580131452dc32d8d0dac42
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-02-02 14:13:22 -0800

    Add code to fetch game descriptions

--- script.js
+++ script.js
@@ -31,14 +31,25 @@ let search = document.querySelector(".search-button");
 search.addEventListener("click", duh);
 let gameList = document.querySelector(".game-list");
 
+async function getGameDescription(gameId) {
+  const response = await fetch(
+    `https://api.rawg.io/api/games/${gameId}?key=652dc6a240454ec7a98d610a0041a14e`
+  );
+  const data = await response.json();
+  return data.description_raw;
+}
+
 async function loopData(gameName) {
   const gameData = await fetchGameData(gameName);
   const gameList = document.querySelector(".game-list");
 
-  gameData.forEach((game) => {
+  gameData.forEach(async (game) => {
     if (game.stores !== null) {
+      // getGameDescription(game.id);
+
       const gameItem = document.createElement("li");
       gameItem.className = "game-item";
+      gameList.appendChild(gameItem);
 
       const gameInfo = document.createElement("article");
       gameInfo.className = "game-data";
@@ -52,15 +63,25 @@ async function loopData(gameName) {
       // ADD CONDITIONAL TO CHECK WHAT STORES EXIST.
       // AND ADD ICONS AND LINKS AS NECCESSARY ACCORDING TO RESULTS
       const storeFronts = document.createElement("div");
+      storeFronts.className = "store-fronts";
       gameInfo.appendChild(storeFronts);
 
       const gameTitle = document.createElement("h2");
-      gameTitle.className = "game-title";
       gameTitle.textContent = game.name;
       storeFronts.appendChild(gameTitle);
 
+      const gameDesc = document.createElement("div");
+      gameDesc.className = "game-desc";
+      gameInfo.appendChild(gameDesc);
+
       const description = document.createElement("p");
-      description.textContent = game.gameList.appendChild(gameItem);
+      const content = await getGameDescription(game.id);
+      description.textContent = content;
+      // console.log(content);
+      gameDesc.appendChild(description);
+      // console.log(description);
+      // const description = document.createElement("p");
+      // description.textContent = getGameDescription(game.id);
       // // console.log(game.name);
     }
   });
