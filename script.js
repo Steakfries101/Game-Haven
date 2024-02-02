d1f78493c5ff7602abd46277fb2ccc49b74fd4f5
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-02-02 10:22:00 -0800

    Create function to test api return data

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const baseUrl = "https://api.rawg.io/api/games/";
-const apiKey = "652dc6a240454ec7a98d610a0041a14e";
+const apiKey = "?key=652dc6a240454ec7a98d610a0041a14e";
 
-let search = document.querySelector(".search-button");
-function duh() {
-  let search = document.querySelector(".search-bar").value;
-  let list = document.getElementById("intro");
-  let h2 = document.createElement("h1");
-  h2.textContent = search;
-  h2.classList = "game-desc"; // Remove the dot before "game-desc"
-  list.appendChild(h2);
+console.log(baseUrl + 54 + apiKey);
+
+// search.addEventListener("click", duh);
+
+// fetchData(gameName);
+
+async function fetchData(gameName) {
+  const response = await fetch(
+    `https://api.rawg.io/api/games?key=652dc6a240454ec7a98d610a0041a14e&search=${gameName}`
+  );
+  const data = await response.json();
+
+  games = data.results;
+  return games;
 }
-search.addEventListener("click", duh);
+async function loopData(gameName) {
+  const gameData = await fetchData(gameName);
+
+  gameData.forEach((game) => console.log(game.name));
+
+  console.log(gameData);
+}
+
+loopData("Hello");
+// let search = document.querySelector(".search-button");
+// function duh() {
+//   let search = document.querySelector(".search-bar").value;
+//   let list = document.getElementById("intro");
+//   let h2 = document.createElement("h1");
+//   h2.textContent = search;
+//   h2.classList = "game-desc"; // Remove the dot before "game-desc"
+//   list.appendChild(h2);
+// }
