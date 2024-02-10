dd37eea4c67d097a0a0ceb92eb81902ea812c337
Evan Shaw <evanshaw34@gmail.com>
2024-02-09 16:56:10 -0800

    Styling read more and read less button

--- script.js
+++ script.js
@@ -27,9 +27,7 @@ async function fetchGameData(gameName) {
 
 async function getGameDescription(gameId) {
   try {
-    const response = await fetch(
-      `https://api.rawg.io/api/games/${gameId}?key=652dc6a240454ec7a98d610a0041a14e`
-    );
+    const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=652dc6a240454ec7a98d610a0041a14e`);
     if (!response.ok) {
       throw new Error("Failed to fetch game desc data");
     }
@@ -51,9 +49,7 @@ async function getGameDescription(gameId) {
 //Get stores for each game based on game id
 async function getGameStores(gameId) {
   try {
-    const response = await fetch(
-      `https://api.rawg.io/api/games/${gameId}/stores?key=652dc6a240454ec7a98d610a0041a14e`
-    );
+    const response = await fetch(`https://api.rawg.io/api/games/${gameId}/stores?key=652dc6a240454ec7a98d610a0041a14e`);
     if (!response.ok) {
       throw new Error("Failed to fetch game stores data");
     }
@@ -79,11 +75,7 @@ async function loopData(gameName) {
   gameData.forEach(async (game) => {
     if (game.store_id !== null) {
       const includesStore = game.stores.some(
-        (store) =>
-          store.store.id === 5 ||
-          store.store.id === 1 ||
-          store.store.id === 2 ||
-          store.store.id === 11
+        (store) => store.store.id === 5 || store.store.id === 1 || store.store.id === 2 || store.store.id === 11
       );
       if (!includesStore) {
         return;
@@ -152,7 +144,7 @@ async function loopData(gameName) {
             const readLess = document.createElement("button");
             readLess.textContent = "Read Less";
             readLess.className = "read-less"; // Add class name for styling
-            part2Para.appendChild(readLess);
+            gameDesc.appendChild(readLess);
 
             // Add event listener for "Read Less" button
             readLess.addEventListener("click", () => {
