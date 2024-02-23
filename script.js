8631e1c1af86712929ee6dab8d93939b5f470737
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-02-23 12:50:59 -0800

    Add try catch to new fetchYoutube function

--- script.js
+++ script.js
@@ -18,14 +18,19 @@ document.getElementById("noRefreshForm").addEventListener("submit", (e) => {
 
 
 async function fetchYoutube(gameName){
-  
+  try{
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${gameName} game trailer&key=AIzaSyCsEU3Fe6wNACeFTvZQgKA46QnreQL12NI`
-    )
+    );if (!response.ok){
+      throw new Error("Failed to fetch youtube data")
+    }
     const youtubeData = await response.json();
-    const trailers = youtubeData.items;
-    // console.log(trailers)
-    return trailers;
+    const videos= youtubeData.items;
+    return videos;
+  }catch (error){
+    console.error("Error fetching youtube data")
+  }
+  
   }
 
 //Get the game data
