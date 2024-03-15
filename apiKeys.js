4dd1085218c18a3d200a40f4c100ddfbbc9e5621
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-03-15 11:51:59 -0700

    Seperate api keys out to own folder

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+export const youtubeKey = "AIzaSyCsEU3Fe6wNACeFTvZQgKA46QnreQL12NI";
+
+//RAWG API
+export const baseUrl = "https://api.rawg.io/api/games";
+export const apiKey = "?key=eb297cab936749b380d022a9e1f0c9f1";
