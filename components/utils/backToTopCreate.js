2923d0f0012182ad758bfc12a72110cd9d961865
Evan Shaw <47705658+Steakfries101@users.noreply.github.com>
2024-03-14 17:10:13 -0700

    Remove uneeded back to top create function

--- components/utils/backToTopCreate.js
+++
@@ -1,9 +0,0 @@
-//Create back to top button
-// export function backToTopCreate(gameData) {
-//   const toTopElement = document.getElementById("to-top");
-//   if (gameData.length > 0) {
-//     toTopElement.style.display = "flex";
-//   } else {
-//     toTopElement.style.display = "none";
-//   }
-// }
